6361f613c645c686897f81e36058531b5f5f3803
mechelle presnell <mechellenpresnell@students.abtech.edu>
2020-09-24 14:06:08 -0400

    fixed asgn02

--- inheritance.php
+++ inheritance.php
@@ -1,11 +1,11 @@
 <?php
 
 class Animal {
-    var $legs;
-    var $can_fly;
+    public $legs;
+    public $can_fly;
 
-    function animal_characteristics() {
-        return $this->legs . ", " . $this->can_fly;
+    public function animal_characteristics() {
+        return $this->legs . '. ' . ' Can it fly? ' . $this->can_fly;
     }
 }
 
@@ -19,44 +19,18 @@ class Bird extends Animal {
 
 $fish = new Animal;
 $fish->legs = 0;
-$fish->can_fly = false;
+$fish->can_fly = 'no';
 
 $husky = new Dog;
 $husky->legs = 4;
-$husky->can_fly = false;
+$husky->can_fly = 'no';
 
 $robin = new Bird;
 $robin->legs = 2;
-$robin->can_fly = true;
+$robin->can_fly = 'yes';
 
-echo $fish->animal_characteristics() . '<br>';
-echo $husky->animal_characteristics() . '<br>';
-echo $robin->animal_characteristics() . '<br>';
-
-echo get_parent_class($fish) . '<br>';
-echo get_parent_class($husky) . '<br>';
-echo get_parent_class($robin) . '<br>';
-
-if(is_subclass_of($fish, 'Animal')) {
-    echo "Instance is a subclass of Animal. <br>";
-}
-
-if(is_subclass_of($husky, 'Dog')) {
-    echo "Instance is a subclass of Animal. <br>";
-}
-
-if(is_subclass_of($robin, 'Bird')) {
-    echo "Instance is a subclass of Animal. <br>";
-}
-
-$parents = class_parents($robin);
-echo implode(', ', $parents) . '<br>';
-
-$parents = class_parents($husky);
-echo implode(', ', $parents) . '<br>';
-
-$parents = class_parents($fish);
-echo implode(', ', $parents) . '<br>';
-
+echo 'The number of legs a fish has is ' . $fish->animal_characteristics() . '<br>';
+echo 'The number of legs a dog has is ' . $husky->animal_characteristics() . '<br>';
+echo 'The number of legs a bird has is ' . $robin->animal_characteristics() . '<br>';
 
 ?>
